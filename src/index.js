7ac4fe3e5a3f0c8cfa1da6f95aca97cba7a5fc20
Arturo Wibawa <agwibawa@gmail.com>
2021-08-06 16:59:51 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "https://unpkg.com/akar-icons-fonts/src/css/akar-icons.css";
+head.appendChild(link);
